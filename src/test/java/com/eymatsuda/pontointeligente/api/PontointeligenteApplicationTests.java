6745904271cd29997d494db1fac3e7d000903938
eymatsuda2016 <eymatsuda2016@outlook.com>
2025-10-05 21:25:03 -0300

    Projeto Ponto Inteligente Criado

---
+++ src/test/java/com/eymatsuda/pontointeligente/api/PontointeligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eymatsuda.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontointeligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
